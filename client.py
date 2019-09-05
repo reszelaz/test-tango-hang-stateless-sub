4ebe9f404cc80023a73de4ea5baff6d7068a2df4
zreszela <zreszela@cells.es>
2019-09-05 17:38:07 +0200

    Simplify example to use directly a tango client

--- client.py
+++ client.py
@@ -1,17 +1,31 @@
 import time
+import threading
 import PyTango
 
 
-DEV_NAME = "test/pydsexpclient/1"
+DEV_NAME = "test/pydsexp/1"
+
+
+class JobThread(threading.Thread):
+
+    def __init__(self):
+        super(JobThread, self).__init__()
+
+    def run(self):
+        attr = PyTango.AttributeProxy(DEV_NAME + "/attr1")
+        attr.write(10)
 
 
 def test():
     dp = PyTango.DeviceProxy(DEV_NAME)
+    dp.subscribe_event("attr2", PyTango.EventType.CHANGE_EVENT,
+                       PyTango.utils.EventCallback(), [], True)
     i = 0
     while True:
         print("i = {}".format(i))
-        dp.command_inout("Start")
-        while dp.State() != PyTango.DevState.ON:
+        thread = JobThread()
+        thread.start()
+        while thread.isAlive():
             time.sleep(0.01)
         i += 1
 
